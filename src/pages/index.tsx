5a9d8d7bceeb5abc244c00206f77e3f06de5d1e3
wachira <wachirastudeo@gmail.com>
2024-09-07 11:21:34 +0700

    แยก component แล้วใช้ props ส่งข้อมูล เพิ่ม type ให้เหมาะสม ใช้ interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,60 @@
-import { ChangeEventHandler,useState } from "react";
+import { ChangeEventHandler, useState } from 'react';
+
+interface TodoFormProps {
+  addTodo:(todo:string) =>void
+}
+
+const TodoForm = ({addTodo}:TodoFormProps) => {
+  const [todo, setTodo] = useState(''); //state ,setState
+  const handleTodoFormchange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setTodo(event.target.value);
+  };
+  const handleAddTodo=()=>{
+    addTodo(todo)
+    setTodo('');
+  }
+  return (
+    <>
+      <input type="text" value={todo} onChange={handleTodoFormchange} />
+      <button onClick={handleAddTodo}>add</button>
+    </>
+  );
+};
+interface Todo{
+  id:number,
+  content:string
+}
+interface TodoListProps{
+  todos:Todo[]
+}
+const TodoList = ({ todos }:TodoListProps) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.content}</li>
+      ))}
+    </ul>
+  );
+};
+
+
+
 const IndexPage = () => {
-  const [todo,setTodo] = useState('') //state ,setState
-  const [todos,setTodos] = useState(
-    [
-      { id: 1, content: 'Todot1' },
-      { id: 2, content: 'Todot2' },
-      { id: 3, content: 'Todot3' },
-    ]
-  )
-  const handleTodoFormchange:ChangeEventHandler<HTMLInputElement>=(event)=>{
-    setTodo(event.target.value)
-  }
-  const addTodo=()=>{
-    setTodos([{id:todos.length+1,content:todo},...todos])
-    setTodo('')
-  }
+  const [todos, setTodos] = useState([
+    { id: 1, content: 'Todot1' },
+    { id: 2, content: 'Todot2' },
+    { id: 3, content: 'Todot3' },
+  ]);
+
+  const addTodo = (todo:string) => {
+    setTodos([{ id: todos.length + 1, content: todo }, ...todos]);
+
+  };
 
   return (
     <>
-    <input type="text" value={todo}  onChange={handleTodoFormchange} />
-    <button onClick={addTodo}>add</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+      <TodoForm addTodo={addTodo}></TodoForm>
+      <TodoList todos={todos}></TodoList>
     </>
   );
 };
