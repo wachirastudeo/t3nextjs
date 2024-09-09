3881459a1830a64c5d2335aa804283eb47b8c669
wachira <wachirastudeo@gmail.com>
2024-09-09 09:33:54 +0700

    query data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,83 +1,22 @@
+import { api } from "~/utils/api";
+const IndexPage=()=>{
+     //useQuery() คือดึงข้อมูลออกมาดูอย่างเดียว
 
-import { fetchData } from "next-auth/client/_utils";
-import { useEffect, useState } from "react";
-
-interface DataItem {
-  id:number
-}
-
-//react hook
-  // T คือ generic รับเข้ามาได้ ที่ต้องมีข้อมูลเหมือน DataItem 
-const useFech =<T extends DataItem>(url:string)=>{    
-  const [datas,setDatas] = useState<T[]>([])   //type DataItem 
-
-  useEffect(()=>{
-    const fetchData = async()=>{
-        const res = await fetch(url)
-        const data = await (res.json() as Promise<T[]>) // promise ที่มี type แบบ DataItem
-        setDatas(data)
-    }
-     fetchData() //   ใส่ void ไว้ด้านหน้าได้   ไม่อยากทำอะไรต่อ
-},[url])
-
-return datas
-}
-
-
-interface User{
-  id:number,
-  name:string
-}
-
-const User=()=>{
-  const url = "https://jsonplaceholder.typicode.com/users"
-  
-   const users= useFech<User>(url) // ส่ง generic interface ประเภทข้อมูล User ไว้กำกับขารับข้อมูลออกมาก
+   const {data:articles,isLoading}= api.article.list.useQuery(); 
+   if(isLoading) return <div>Loading</div>
+   if(!articles) return <div>No Content</div>
 
   return(
-    <>
-    <ul>
-      {
-        users.map(user => <li key={user.id}>{user.name}</li>)
-      }
-      </ul>
-    </>
-  )
-}
-
-interface Todo{
-  id:number,
-  title:string
-}
-
-const Todo=()=>{
-  const url = "https://jsonplaceholder.typicode.com/todos"
-  
-  const todos= useFech<Todo>(url)
-  return(
-    <>
-    <ul>
-      {
-        todos.map(todo => <li key={todo.id}>{todo.title}</li>)
-      }
-      </ul>
-    </>
-  )
-
-}
-
-const IndexPage = () => {
- 
- return(
   <>
-  <h1 >Users</h1>
- <User></User>
- <br></br>
- <h1>TODOs</h1>
- <Todo></Todo>
-  </>
- )
-};
+  
+    <ul>
+      {
+        articles.map((article) => (
 
+        <li key={article.id}>{article.title}</li>
+
+        ))}
+    </ul>
+  </>)
+}
 export default IndexPage;
-
