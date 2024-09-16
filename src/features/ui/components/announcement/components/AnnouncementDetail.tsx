363bc77bca7a6143067e70c46e74400faebece06
wachira <wachirastudeo@gmail.com>
2024-09-16 20:30:26 +0700

    announcementDetail

--- src/features/ui/components/announcement/components/AnnouncementDetail.tsx
+++ src/features/ui/components/announcement/components/AnnouncementDetail.tsx
@@ -2,10 +2,15 @@ import { useRouter } from "next/router";
 import { api } from "~/utils/api";
 import Loading from "../../Loading";
 import Card from "../../Card";
+import { Announcement } from "../types";
 
-const AnnouncementDetail = () => {
-    const router = useRouter() // ดึงค่า ตรง url ได้ ไว้ทำการ query
-    const {data:announcement,isLoading} = api.announcement.bySlug.useQuery(router.query.slug as string);
+export interface AnnouncementDetailProps{
+    slug:Announcement['slug'];
+}
+
+const AnnouncementDetail = ({slug}:AnnouncementDetailProps) => {
+    // const router = useRouter() // ดึงค่า ตรง url ได้ ไว้ทำการ query
+    const {data:announcement,isLoading} = api.announcement.bySlug.useQuery(slug);
     if(isLoading)return<Loading></Loading>
     if(!announcement) return<>Not Found</>
     return ( 
--- src/pages/announcements/[slug].tsx
+++ src/pages/announcements/[slug].tsx
@@ -1,8 +1,24 @@
 import AnnouncementDetail from "~/features/ui/components/announcement/components/AnnouncementDetail";
 import Layout from "~/features/ui/components/layouts/Normal";
+import { generateServerSideHelper } from "~/server/shared/serverSideHelper";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-const DetailPage = () => {
-    return <AnnouncementDetail></AnnouncementDetail>
+export const getServerSideProps:GetServerSideProps<{slug:string}> =async(context)=>{ 
+    const slug = context.params?.slug as string;
+    const helpers = generateServerSideHelper()
+    await helpers.announcement.bySlug.prefetch(slug)
+
+    return {
+        props:{
+            trpcStat: helpers.dehydrate(),
+            slug,
+        }
+    }
+  
+  }
+
+const DetailPage = ({slug}:InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    return <AnnouncementDetail slug={slug}></AnnouncementDetail>
 }
  
 DetailPage.getLayout = Layout
