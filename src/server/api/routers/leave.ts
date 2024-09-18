be86cc80d42ea525fa79403865016a08664fb690
wachira <wachirastudeo@gmail.com>
2024-09-18 20:06:12 +0700

    leave prisma

--- src/server/api/routers/leave.ts
+++ src/server/api/routers/leave.ts
@@ -6,28 +6,16 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
-interface Leave{
-  id:number;
-  reason:string;
-  status:'PENDING'| 'APPROVED' | 'REJECTED';
-  leaveDate:string;
- 
-}
 
 
 
-const leave:Leave[]=[
-  {id:1,reason:'reason  #1',status:'PENDING',leaveDate:new Date().toISOString()},
-  {id:2,reason:'reason  #2',status:'APPROVED',leaveDate:new Date().toISOString()},
-  {id:3,reason:'reason  #3',status:'REJECTED',leaveDate:new Date().toISOString()},
 
-]
 
-// fontend => api.post.hello 
  // api.article.[list, byId,update,delete,add] สร้างชื่อตามที่ต้องการได้
 //backend สร้างแล้วให้ fontend ไปใช้ชื่อเดียวกัน
 export const leaveRouter = createTRPCRouter({
-  list:publicProcedure.query(()=>{  //procedure ใครจะเข้ามาอ่านก็ได้ 
+  list:publicProcedure.query(({ctx})=>{  //procedure ใครจะเข้ามาอ่านก็ได้ 
+      const leave = ctx.prisma.leave.findMany();
       return leave
   }),
  
